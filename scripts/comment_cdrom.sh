8fda3bb9fbab589e4652d1e1698e71ced4da8c08
steugen <p.gnomme@gmail.com>
2024-07-24 12:35:39 +0400

    removing password with sudo in preseed

--- scripts/comment_cdrom.sh
+++ scripts/comment_cdrom.sh
@@ -4,8 +4,8 @@ SOURCES_LIST="/etc/apt/sources.list"
 
 if [ -f "$SOURCES_LIST" ]; then
 
-	echo packer | sudo sed -i '/^deb cdrom/ s/^/#/' "$SOURCES_LIST"
-	echo packer | sudo apt update
+	sudo sed -i '/^deb cdrom/ s/^/#/' "$SOURCES_LIST"
+	sudo apt update
 	echo "Lines containing 'deb cdrom' have been commented out in $SOURCES_LIST."
 else
 	echo "File $SOURCES_LIST does not exist."
